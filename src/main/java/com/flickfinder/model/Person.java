0718d4b578820be186516254591505a468de079c
sl02297 <sl02297@surrey.ac.uk>
2024-05-03 17:22:05 +0100

    added person and movie objects

--- src/main/java/com/flickfinder/model/Person.java
+++ src/main/java/com/flickfinder/model/Person.java
@@ -11,5 +11,36 @@ public class Person {
 	// - Check the ERD and database schema in the docs folder
 	// (./docs/database_schema.md) to ensure each column in the People table
 	// has an attribute in the model. (DELETE THIS COMMENT WHEN DONE)
+	
+	private int id;
+	private String name;
+	private int birth;
+	
+	public Person(int id, String name, int birth) {
+		this.id = id;
+		this.name = name;
+		this.birth = birth;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getBirth() {
+		return this.birth;
+	}
+	public void setBirth(int birth) {
+		this.birth = birth;
+	}
 
 }
